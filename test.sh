1b7980380a91d642f6722fc56093ca9446176a01
sculpt0r <b.kalemba@cksource.com>
2021-02-09 00:05:19 +0100

    Reorder commands

--- test.sh
+++ test.sh
@@ -16,10 +16,16 @@
 
 bSha=$( gh api /repos/ckeditor/workflow-tests-PR-6/git/refs/heads | jq -c '.[] | select(.ref | contains("refs/heads/master"))' | jq -c -r '.object.sha' )
 
-echo ${bSha}
+#echo ${bSha}
 
+#new branch from clear repo - next step require SHA of updated file if there is tsth to update - don't want this
 #create branch on workflow test repo
 gh api --method POST -F ref="refs/heads/testBRANCH" -F sha=${bSha} /repos/ckeditor/workflow-tests-PR-6/git/refs
+
+#put all files in workflow to testing repo new branch '.github/workflows` & cfg file to `.github`
+#https://docs.github.com/en/rest/reference/repos#create-or-update-file-contents
+stalebotContent=$( base64 workflows/stalebot.yml )
+gh api --method PUT -F branch="testBRANCH" -F message="update stalebot" /repos/ckeditor/workflow-tests-PR-6/contents/.github/workflows/stalebot.yml -F content="${stalebotContent}"
 
 
 
