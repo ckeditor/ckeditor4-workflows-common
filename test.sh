e5fa63c14c58660814e677345af235e35f3657d9
sculpt0r <b.kalemba@cksource.com>
2021-02-09 14:23:55 +0100

    Iterate over received actions

--- test.sh
+++ test.sh
@@ -1,7 +1,22 @@
-#create empty repo
-#add selected files to .github/workflows & add settings if provided
-#via gh api - run workflow & watch results
+#https://docs.github.com/en/rest/reference/repos#list-commits
+#https://docs.github.com/en/rest/reference/repos#get-a-commit
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Get last commit sha in master branch
 # bSha=$( gh api /repos/ckeditor/workflow-tests-PR-6/git/refs/heads | jq -c '.[] | select(.ref | contains("refs/heads/master"))' | jq -c -r '.object.sha' )
 
 #echo ${bSha}
@@ -10,17 +25,27 @@
 #create branch on workflow test repo
 #gh api --method POST -F ref="refs/heads/testBRANCH" -F sha=${bSha} /repos/ckeditor/workflow-tests-PR-6/git/refs
 
+
+
+
+
 #put all files in workflow to testing repo new branch '.github/workflows` & cfg file to `.github`
 #https://docs.github.com/en/rest/reference/repos#create-or-update-file-contents
 #stalebotContent=$( base64 workflows/stalebot.yml )
 #gh api --method PUT -F branch="testBRANCH" -F message="update stalebot" /repos/ckeditor/workflow-tests-PR-6/contents/.github/workflows/stalebot.yml -F content="${stalebotContent}"
 
 
+# Get actions list
 # https://stedolan.github.io/jq/download/
-gh api /repos/ckeditor/workflow-tests-PR-6/actions/workflows | jq -c ".workflows | map({ name: .name, id: .id }) | .[]"
+actionList=$( gh api /repos/ckeditor/workflow-tests-PR-6/actions/workflows | jq -c ".workflows | map(.id) | .[]" )
+# echo ${actionList}
 
-#dispatch workflows
-#gh api --method POST -F ref="master" /repos/ckeditor/workflow-tests-PR-6/actions/workflows/5661822/dispatches | jq -c ".workd"
+for actionId in "${actionList[@]}"
+do
+    #dispatch workflows
+    gh api --method POST -F ref="master" /repos/ckeditor/workflow-tests-PR-6/actions/workflows/$actionId/dispatches | jq -c ".workd"
+done
+
 
 #list running actions
 #https://docs.github.com/en/rest/reference/actions#list-workflow-runs-for-a-repository
