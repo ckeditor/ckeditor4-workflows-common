cb79c2d96ca23e3267e3c357add681f8c23249ba
sculpt0r <b.kalemba@cksource.com>
2021-02-11 14:53:42 +0100

    Reset repo master branch to hardcoded commit

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
+# Assume that testing repo has first "empty" commit
+
 #https://docs.github.com/en/rest/reference/repos#list-commits
 #https://docs.github.com/en/rest/reference/repos#get-a-commit
 
@@ -19,11 +21,25 @@
 # Get last commit sha in master branch
 # bSha=$( gh api /repos/ckeditor/workflow-tests-PR-6/git/refs/heads | jq -c '.[] | select(.ref | contains("refs/heads/master"))' | jq -c -r '.object.sha' )
 
-#echo ${bSha}
+# Hardcoded initial empty commit
+bSha="ae15364ec3c7cceb7a0624284e4627cc1088ad99"
+
+echo ${bSha}
 
 #new branch from clear repo - next step require SHA of updated file if there is tsth to update - don't want this
 #create branch on workflow test repo
 #gh api --method POST -F ref="refs/heads/testBRANCH" -F sha=${bSha} /repos/ckeditor/workflow-tests-PR-6/git/refs
+
+#Reset master branch to initial commit
+gh api --method PATCH -F sha=$bSha -F force="true" /repos/ckeditor/workflow-tests-PR-6/git/refs/heads/master
+
+
+
+
+
+
+
+
 
 
 
@@ -40,36 +56,13 @@ do
     #make a proper url and send!
     apiUrl="/repos/ckeditor/workflow-tests-PR-6/contents/.github/${file:1}"
     #echo $apiUrl 
-    gh api --method PUT $apiUrl -F branch="testBRANCH" -F message="Update workflow file" -F content="${stalebotContent}"
+    #gh api --method PUT $apiUrl -F branch="testBRANCH" -F message="Update workflow file" -F content="${stalebotContent}"
 done
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 # Get actions list
 # actionList=$( gh api /repos/ckeditor/workflow-tests-PR-6/actions/workflows | jq -c ".workflows | map(.id) | .[]" )
 
+# dispatach all available workflows
 # for actionId in "${actionList[@]}"
 # do
 #     #dispatch workflows
