fbd3e201478368aed89340697fb5cea9c286c5ad
sculpt0r <b.kalemba@cksource.com>
2021-02-12 12:55:20 +0100

    Parse GH output

--- test.sh
+++ test.sh
@@ -1,19 +1,20 @@
 # Assume that testing repo has first "empty" commit
+TESTING_REPO="ckeditor/workflow-tests-PR-6"
 
 # Hardcoded initial empty commit
 bSha="ae15364ec3c7cceb7a0624284e4627cc1088ad99"
 
 printf "\nReset master branch to initial commit\n"
-gh api --method PATCH -F sha=$bSha -F force="true" /repos/ckeditor/workflow-tests-PR-6/git/refs/heads/master
+gh api --method PATCH -F sha=$bSha -F force="true" /repos/$TESTING_REPO/git/refs/heads/master
 
 printf "\nGather running actions\n"
 #https://docs.github.com/en/rest/reference/actions#list-workflow-runs-for-a-repository
-repoActions=$( gh api /repos/ckeditor/workflow-tests-PR-6/actions/runs | jq -c ".workflow_runs | map(.id) | .[]" )
+repoActions=$( gh api /repos/$TESTING_REPO/actions/runs | jq -c ".workflow_runs | map(.id) | .[]" )
 
 printf "\nDelete all existing actions\n"
 for actionId in ${repoActions[@]}
 do
-  apiUrl="/repos/ckeditor/workflow-tests-PR-6/actions/runs/${actionId}"
+  apiUrl="/repos/$TESTING_REPO/actions/runs/${actionId}"
   gh api --method DELETE ${apiUrl}
 done
 
@@ -25,17 +26,28 @@ workflowFiles=( ./workflows/*)
 
 for file in ${workflowFiles[@]}
 do
-    stalebotContent=$( base64 $file )
-    apiUrl="/repos/ckeditor/workflow-tests-PR-6/contents/.github/${file:2}"
-    echo $apiUrl
-    gh api --method PUT $apiUrl -F branch="master" -F message="Update ${file:2} file" -F content="${stalebotContent}"
+  fileContent=$( base64 $file )
+  apiUrl="/repos/$TESTING_REPO/contents/.github/${file:2}"
+  echo $apiUrl
+  result=$( gh api --method PUT $apiUrl -F branch="master" -F message="Add ${file:2} file" -F content="${fileContent}" | jq -c ".content.name" )
+  echo $result
 done
 
+FILE=./workflows-config.json
+if [ -f "$FILE" ]; then
+  printf "\nCommit config\n"
+  cfgContent=$( base64 $FILE )
+  apiUrl="/repos/$TESTING_REPO/contents/.github/${FILE:2}"
+  echo $apiUrl
+  result=$( gh api --method PUT $apiUrl -F branch="master" -F message="Add config: $FILE" -F content="${cfgContent}" | jq -c ".content.name" )
+  echo $result
+fi
+
+printf "\nGather actions...\n"
 sleep 3
 
-printf "\nGather actions\n"
 # Get actions list
-actionList=$( gh api /repos/ckeditor/workflow-tests-PR-6/actions/workflows | jq -c ".workflows | map(.id) | .[]" )
+actionList=$( gh api /repos/$TESTING_REPO/actions/workflows | jq -c ".workflows | map(.id) | .[]" )
 
 echo $actionList
 
@@ -43,8 +55,11 @@ printf "\nDispatach actions\n"
 # Dispatach all available workflows
 for actionId in ${actionList[@]}
 do
-    #dispatch workflows
-    apiUrl="/repos/ckeditor/workflow-tests-PR-6/actions/workflows/${actionId}/dispatches"
-    gh api --method POST -F ref="master" $apiUrl
+  #dispatch workflows
+  apiUrl="/repos/$TESTING_REPO/actions/workflows/${actionId}/dispatches"
+  gh api --method POST -F ref="master" $apiUrl
 done
 
+echo "Done!"
+
+echo " Please, visit https://github.com/$TESTING_REPO/actions to verify results."
