57097848cec2fd0ffc5d97dc7d32e7aeec1c5977
sculpt0r <b.kalemba@cksource.com>
2021-02-12 09:49:12 +0100

    Display steps, order actions

--- test.sh
+++ test.sh
@@ -1,75 +1,47 @@
 # Assume that testing repo has first "empty" commit
 
-#https://docs.github.com/en/rest/reference/repos#list-commits
-#https://docs.github.com/en/rest/reference/repos#get-a-commit
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Get last commit sha in master branch
-# bSha=$( gh api /repos/ckeditor/workflow-tests-PR-6/git/refs/heads | jq -c '.[] | select(.ref | contains("refs/heads/master"))' | jq -c -r '.object.sha' )
-
 # Hardcoded initial empty commit
 bSha="ae15364ec3c7cceb7a0624284e4627cc1088ad99"
 
-echo ${bSha}
-
-#new branch from clear repo - next step require SHA of updated file if there is tsth to update - don't want this
-#create branch on workflow test repo
-#gh api --method POST -F ref="refs/heads/testBRANCH" -F sha=${bSha} /repos/ckeditor/workflow-tests-PR-6/git/refs
-
-#Reset master branch to initial commit
+printf "\nReset master branch to initial commit\n"
 gh api --method PATCH -F sha=$bSha -F force="true" /repos/ckeditor/workflow-tests-PR-6/git/refs/heads/master
 
+printf "\nGather running actions\n"
+#https://docs.github.com/en/rest/reference/actions#list-workflow-runs-for-a-repository
+repoActions=$( gh api /repos/ckeditor/workflow-tests-PR-6/actions/runs | jq -c ".workflow_runs | map(.id) | .[]" )
 
+printf "\nDelete all existing actions\n"
+for actionId in ${repoActions[@]}
+do
+  apiUrl="/repos/ckeditor/workflow-tests-PR-6/actions/runs/${actionId}"
+  gh api --method DELETE ${apiUrl}
+done
 
+printf "\nCommit Files\n"
+# Put all files from workflow to testing repo branch directory '.github/workflows`
+# https://docs.github.com/en/rest/reference/repos#create-or-update-file-contents
 
-
-
-
-
-
-
-
-
-
-#put all files in workflow to testing repo new branch '.github/workflows` & cfg file to `.github`
-#https://docs.github.com/en/rest/reference/repos#create-or-update-file-contents
 workflowFiles=( ./workflows/*)
 
 for file in ${workflowFiles[@]}
 do
     stalebotContent=$( base64 $file )
-    # echo $file
-    #make a proper url and send!
-    apiUrl="/repos/ckeditor/workflow-tests-PR-6/contents/.github/${file:1}"
-    #echo $apiUrl 
-    #gh api --method PUT $apiUrl -F branch="testBRANCH" -F message="Update workflow file" -F content="${stalebotContent}"
+    apiUrl="/repos/ckeditor/workflow-tests-PR-6/contents/.github/${file:2}"
+    echo $apiUrl
+    gh api --method PUT $apiUrl -F branch="master" -F message="Update ${file:2} file" -F content="${stalebotContent}"
 done
 
+printf "\nGather actions\n"
 # Get actions list
-# actionList=$( gh api /repos/ckeditor/workflow-tests-PR-6/actions/workflows | jq -c ".workflows | map(.id) | .[]" )
+actionList=$( gh api /repos/ckeditor/workflow-tests-PR-6/actions/workflows | jq -c ".workflows | map(.id) | .[]" )
 
-# dispatach all available workflows
-# for actionId in "${actionList[@]}"
-# do
-#     #dispatch workflows
-#     gh api --method POST -F ref="master" /repos/ckeditor/workflow-tests-PR-6/actions/workflows/$actionId/dispatches | jq -c ".workd"
-# done
+echo $actionList
+printf "\nDispatach actions\n"
+# Dispatach all available workflows
+for actionId in "${actionList[@]}"
+do
+    #dispatch workflows
+    gh api --method POST -F ref="master" /repos/ckeditor/workflow-tests-PR-6/actions/workflows/$actionId/dispatches
+    #| jq -c ".workd"
+done
 
-
-#list running actions
-#https://docs.github.com/en/rest/reference/actions#list-workflow-runs-for-a-repository
-#gh api /repos/ckeditor/workflow-tests-PR-6/actions/runs | jq -c ".workflow_runs | map({ name: .name, id: .id, status: .status, conclusion: .conclusion }) | .[]"
