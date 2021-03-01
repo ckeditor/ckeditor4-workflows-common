8cffe57f04d2d6f4bf9fa92a60ba2b49749096a3
sculpt0r <b.kalemba@cksource.com>
2021-03-01 14:46:46 +0100

    WIP: not working git add

--- before-each.js
+++ before-each.js
@@ -7,14 +7,37 @@ const { spawn } = require("child_process");
 
 const tmpRepoPath = path.join( process.cwd(), 'tmp' );
 
-runCmd( 'rm', [ '-r', '-f', 'tmp' ] )
-    .then( runCmd( 'git', ['clone', process.env.TEST_REPO, 'tmp' ] ) )
-    .then( runCmd( 'rm', [ '-r', '-f', '*' ], { cwd: tmpRepoPath } ) )
-    .then( runCmd( 'git', [ 'add', '.'], { cwd: tmpRepoPath } ) )
-    .then( runCmd( 'git', [ 'commit', '-m', 'Add file' ], { cwd: tmpRepoPath } ) )
-    .then( runCmd( 'git', [ 'push', 'origin', 'master' ], { cwd: tmpRepoPath } ) );
+const filesList = [
+        {
+            src: 'workflows/setup-workflows.yml',
+            dest: '.github/workflows/setup-workflows.yml' 
+        },
+        {
+            src: 'workflows-config.json',
+            dest: '.github/workflows-config.json' 
+        },
+]
+console.log(tmpRepoPath);
 
-function runCmd(cmd, args, options) {
+// .then(x =>  runCmd( 'rm', [ '-rf', '*' ], { cwd: tmpRepoPath } ) )
+runCmd( 'rm', [ '-rf', 'tmp' ] )
+    .then(x => runCmd( 'git', ['clone', process.env.TEST_REPO, 'tmp' ] ) )
+    .then( () => {
+        filesList.forEach( entry => {
+            fs.copyFileSync( entry.src, path.join(tmpRepoPath, entry.dest ) );
+        } );
+        return Promise.resolve();
+    })
+    .then (() => {
+        process.chdir('tmp')
+        return Promise.resolve();
+    })
+    .then( runCmd( 'git', [ 'add', '.'], { cwd: tmpRepoPath } ) )
+    // .then( runCmd( 'git', [ 'commit', '-m', 'Add file' ], { cwd: tmpRepoPath } ) )
+    // .then( runCmd( 'git', [ 'push', 'origin', 'master' ], { cwd: tmpRepoPath } ) );
+    .then( runCmd( 'git', [ 'remote', 'show', 'origin' ], { cwd: tmpRepoPath } ) );
+
+    function runCmd(cmd, args , options) {
     return new Promise((resolve, reject) => {
         const command = spawn( cmd,  args, options );
 
@@ -31,7 +54,8 @@ function runCmd(cmd, args, options) {
         });
     
         command.on("close", code => {
-            console.log(`${cmd} child process exited with code ${code}`);
+            console.log(...args);
+            console.log(`-- -- exited with code ${code}`);
             resolve('ok')
         });
     });
