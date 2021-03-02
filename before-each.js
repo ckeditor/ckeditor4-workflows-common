87ef0be18fffb8458ad026ad99fe2bc2a56cb213
sculpt0r <b.kalemba@cksource.com>
2021-03-02 14:14:46 +0100

    Clean up code, removed unused code, fix intendation

--- before-each.js
+++ before-each.js
@@ -1,92 +1,73 @@
 
 require( 'dotenv' ).config();
 
-const fs = require( 'fs' )
-const path = require( 'path' )
-const { spawn } = require("child_process");
-const { request } = require( '@octokit/request' );
-const { rejects } = require('assert');
-
-const tmpRepoPath = path.join( process.cwd(), 'tmp' );
-
-const filesList = [
-        {
-            src: 'workflows/setup-workflows.yml',
-            dest: '.github/workflows/setup-workflows.yml' 
-        },
-        {
-            src: 'workflows-config.json',
-            dest: '.github/workflows-config.json' 
-        },
-]
+const fs = require( 'fs' ),
+	path = require( 'path' ),
+	{ request } = require( '@octokit/request' );
 
 async function CommitFile( sha, content, filePath, branch ) {
-    const result = await request( 'PUT /repos/{owner}/{repo}/contents/{path}', {
-        headers: {
-            authorization: 'token ' + process.env.AUTH_KEY
-        },
-        owner: process.env.OWNER ,
-        repo: process.env.REPO,
-        path: filePath,
-        branch: branch,
-        sha: sha,
-        content: content,
-        message: 'Update file ' + filePath,
-      });
+	const result = await request( 'PUT /repos/{owner}/{repo}/contents/{path}', {
+		headers: {
+			authorization: 'token ' + process.env.AUTH_KEY
+		},
+		owner: process.env.OWNER ,
+		repo: process.env.REPO,
+		path: filePath,
+		branch: branch,
+		sha: sha,
+		content: content,
+		message: 'Update file ' + filePath,
+	} );
 
-    return result;
+	return result;
 }
 
 async function GetFile( path, branch ) {
-    const result = await request( 'GET /repos/{owner}/{repo}/contents/{path}?ref={ref}', {
-        headers: {
-            authorization: 'token ' + process.env.AUTH_KEY
-        },
-        owner: process.env.OWNER ,
-        repo: process.env.REPO,
-        path: path,
-        ref: branch,
-      });
+	const result = await request( 'GET /repos/{owner}/{repo}/contents/{path}?ref={ref}', {
+		headers: {
+			authorization: 'token ' + process.env.AUTH_KEY
+		},
+		owner: process.env.OWNER ,
+		repo: process.env.REPO,
+		path: path,
+		ref: branch,
+	} );
 
-    return result;
+	return result;
 }
 
 function SendFile(branch, sourceFilePath, destinationFilePath) {
-    return new Promise((res, rej) => {
+	return new Promise( ( res, rej ) => {
 
-    const newContent = fs.readFileSync(sourceFilePath, {
-        encoding: 'base64'
-    });
-        GetFile(destinationFilePath, branch)
-        .then( res => res.data.sha)
-        .then( sha => {
+	const newContent = fs.readFileSync( sourceFilePath, {
+		encoding: 'base64'
+	} );
 
-            CommitFile(sha, newContent, destinationFilePath, branch)
-            .then(result => {
-                res(result)
-            })
-            .catch(why => {
-                console.log(why);
-                console.log( 'an catch occured', why.status, why.name)
-                
-            } );
-
-
-        } )
-        .catch(why => console.warn( 'File not found' ) );
-    } )
+	GetFile( destinationFilePath, branch )
+		.then( res => res.data.sha)
+		.then( sha => {
+			CommitFile( sha, newContent, destinationFilePath, branch )
+				.then( result => {
+					res( result );
+				})
+				.catch( why => {
+					console.log( why );
+					console.log( 'An catch occured', why.status, why.name)
+				} );
+		} )
+		.catch( why => console.warn( 'File not found' ) );
+	} );
 }
 
 function SendFiles( branch, files ) {
-console.log('runned wiht', files);
-    const nextFile = files.shift();
+	const nextFile = files.shift();
 
-    if(nextFile) {
-        return SendFile( branch, nextFile.src, nextFile.dest )
-                .then( r => SendFiles( branch, files ) );
-    } else {
-        return Promise.resolve();
-    }
+	if( nextFile ) {
+		return SendFile( branch, nextFile.src, nextFile.dest )
+				.then( r => SendFiles( branch, files ) );
+	} else {
+		return Promise.resolve();
+	}
 }
 
 module.exports = SendFiles;
--- index.js
+++ index.js
@@ -5,70 +5,70 @@ require( 'dotenv' ).config();
 const SendFiles = require( './before-each' );
 
 const tests = {
-    'test setup-workflows': {
-        name: 'setup-workflows.yml',
-        branch: 'master',
-        filesList: [
-            {
-                src: 'workflows/setup-workflows.yml',
-               dest: '.github/workflows/setup-workflows.yml' 
-            },
-            {
-                src: 'workflows-config.json',
-                dest: '.github/workflows-config.json' 
-            },
-        ]
-    }
+	'test setup-workflows': {
+		name: 'setup-workflows.yml',
+		branch: 'master',
+		filesList: [
+			{
+				src: 'workflows/setup-workflows.yml',
+			   dest: '.github/workflows/setup-workflows.yml'
+			},
+			{
+				src: 'workflows-config.json',
+				dest: '.github/workflows-config.json'
+			},
+		]
+	}
 };
 
 for( const testName in tests ) {
-    console.log( 'Running: ' + testName );
-    const testCase = tests[ testName ];
+	console.log( 'Running: ' + testName );
+	const testCase = tests[ testName ];
 
-   debugger 
-    SendFiles( testCase.branch, testCase.filesList)
-    .then( x => {
-        console.log( 'all sends finished', x );
+   debugger
+	SendFiles( testCase.branch, testCase.filesList)
+	.then( x => {
+		console.log( 'all sends finished', x );
 
-        DispatchWorkflow( testCase.name,testCase.branch )
-            .then( res => {
-                // GH need some time before actually workflow is available as `quened`
-                setTimeout( () => {
-                    GetRunningActions()
-                    .then( results => {
-                        const wf =results.data.workflow_runs[0];
-                        // const { id, conclusion, status, name, created_at, updated_at } = wf;
-                        RecheckAction( wf);
-                    } );
-                }, 1000);
-            } )
-            .catch( why => console.log( 'ERROR', why ) );
-    } );
+		DispatchWorkflow( testCase.name,testCase.branch )
+			.then( res => {
+				// GH need some time before actually workflow is available as `quened`
+				setTimeout( () => {
+					GetRunningActions()
+					.then( results => {
+						const wf =results.data.workflow_runs[0];
+						// const { id, conclusion, status, name, created_at, updated_at } = wf;
+						RecheckAction( wf);
+					} );
+				}, 1000);
+			} )
+			.catch( why => console.log( 'ERROR', why ) );
+	} );
 }
 
 async function GetRunningActions() {
-    const result = await request( 'GET /repos/{owner}/{repo}/actions/runs', {
-        headers: {
-            authorization: 'token ' + process.env.AUTH_KEY
-        },
-        owner: process.env.OWNER ,
-        repo: process.env.REPO
-      });
+	const result = await request( 'GET /repos/{owner}/{repo}/actions/runs', {
+		headers: {
+			authorization: 'token ' + process.env.AUTH_KEY
+		},
+		owner: process.env.OWNER ,
+		repo: process.env.REPO
+	  });
 
-    return result;
+	return result;
 }
 
 async function GetWorkflowRun( workflowId ) {
-    const result = await request( 'GET /repos/{owner}/{repo}/actions/runs/{run_id}', {
-        headers: {
-            authorization: 'token ' + process.env.AUTH_KEY
-        },
-        owner: process.env.OWNER ,
-        repo: process.env.REPO,
-        run_id: workflowId
-      });
+	const result = await request( 'GET /repos/{owner}/{repo}/actions/runs/{run_id}', {
+		headers: {
+			authorization: 'token ' + process.env.AUTH_KEY
+		},
+		owner: process.env.OWNER ,
+		repo: process.env.REPO,
+		run_id: workflowId
+	  });
 
-    return result;
+	return result;
 }
 
 let allCompletedActions = [];
@@ -77,34 +77,34 @@ let dots = '.';
 let waitingTime = 1000;
 
 function RecheckAction(wfObject) {
-    if(wfObject.status === "completed"){
-        console.log("Completed &&  ::: " + wfObject.conclusion );
-        return;
-    }
-    dots += '.'
-    waitingTime += 3500;
-    console.log( wfObject.status + ' :: ' + wfObject.conclusion );
-    console.log( 'Let me check again' + dots);
+	if(wfObject.status === "completed"){
+		console.log("Completed &&  ::: " + wfObject.conclusion );
+		return;
+	}
+	dots += '.'
+	waitingTime += 3500;
+	console.log( wfObject.status + ' :: ' + wfObject.conclusion );
+	console.log( 'Let me check again' + dots);
 
-    GetWorkflowRun(wfObject.id)
-        .then( workflow => {
-            // Give some time between rechecks
-            setTimeout( () => {
-                RecheckAction( workflow.data );
-            }, waitingTime);
-        });
+	GetWorkflowRun(wfObject.id)
+		.then( workflow => {
+			// Give some time between rechecks
+			setTimeout( () => {
+				RecheckAction( workflow.data );
+			}, waitingTime);
+		});
 }
 
 async function DispatchWorkflow( workflowId, branch ) {
-    const result = await request( 'POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
-        headers: {
-            authorization: 'token ' + process.env.AUTH_KEY
-        },
-        owner: process.env.OWNER ,
-        repo: process.env.REPO,
-        workflow_id: workflowId,
-        ref: branch
-      });
+	const result = await request( 'POST /repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches', {
+		headers: {
+			authorization: 'token ' + process.env.AUTH_KEY
+		},
+		owner: process.env.OWNER ,
+		repo: process.env.REPO,
+		workflow_id: workflowId,
+		ref: branch
+	  });
 
-    return result;
+	return result;
 }
