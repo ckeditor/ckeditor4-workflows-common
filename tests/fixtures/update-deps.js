9247d90e92461be5356b9babd6b0b5d43df14054
Dariusz Jarzębski <d.jarzebski@cksource.com>
2021-05-27 14:55:04 +0200

    Add more test cases for 'update-deps' workflow.

--- tests/fixtures/update-deps.js
+++ tests/fixtures/update-deps.js
@@ -1,14 +1,51 @@
-module.exports = [ {
-	name:'update-deps PR',
-	workflow:'update-deps.yml',
-	branch: 'master',
-	config: {
-		'updateDeps': {
-			'targetBranch': 'master'
+module.exports = [
+	{
+		name: 'update-deps branch without package.json file',
+		workflow: 'update-deps.yml',
+		branch: 'master',
+		config: {
+			'updateDeps': {
+				'targetBranch': 'master'
+			}
 		}
-	},
-	fileList: [ {
-		src: 'deps-package.json',
-		dest: 'package.json'
-	} ]
-} ];
+	}, {
+		name: 'update-deps on up-to-date branch',
+		workflow: 'update-deps.yml',
+		branch: 'master',
+		config: {
+			'updateDeps': {
+				'targetBranch': 'master'
+			}
+		},
+		fileList: [ {
+			src: 'deps-package-up-to-date.json',
+			dest: 'package.json'
+		} ]
+	}, {
+		name: 'update-deps PR',
+		workflow: 'update-deps.yml',
+		branch: 'master',
+		config: {
+			'updateDeps': {
+				'targetBranch': 'master'
+			}
+		},
+		fileList: [ {
+			src: 'deps-package-outdated.json',
+			dest: 'package.json'
+		} ]
+	}, {
+		name: 'update-deps on non-existing branch',
+		workflow: 'update-deps.yml',
+		branch: 'master',
+		config: {
+			'updateDeps': {
+				'targetBranch': 'main'
+			}
+		},
+		fileList: [ {
+			src: 'deps-package-outdated.json',
+			dest: 'package.json'
+		} ]
+	}
+];
